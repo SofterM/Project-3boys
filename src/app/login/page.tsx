5dcfa96e29116e030db77f95982be3f8dc07c720
SofterM <152159162+SofterM@users.noreply.github.com>
2025-03-31 22:47:52 +0700

    fix

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -39,7 +39,6 @@ const LoginForm = () => {
 
     try {
       setLoading(true);
-      // Simulate API call
       await new Promise(resolve => setTimeout(resolve, 1000));
       console.log('ข้อมูลที่ส่ง:', formData);
       router.push('/form');
